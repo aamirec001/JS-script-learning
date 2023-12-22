aa040615fdbfdf5ac71c67543290cafee300951b
aamirec001 <46120517+aamirec001@users.noreply.github.com>
2023-12-22 16:04:25 +0000

    new learning 22nd Dec

---
+++ 01_basics/string2Angelanotes.js
@@ -0,0 +1,40 @@
+// var firstName = "Aamir"
+// var lastName =  prompt("What's your secondName?");
+
+// to prompt end user - prompt("What's your secondName?"); This is possible only when we are coding in web browser, prompt doesnot work in nodejs, for node js we have to use readline
+
+// console.log(` My name is ${firstName} ${lastName}`);
+// alert(` My name is ${firstName} ${lastName}`);
+
+// to create alert - alert(` My name is ${firstName} ${lastName}`);
+
+
+/* ********************************************String Exercise***********
+
+var opinion = prompt ("Give ur opinion");
+var totalChar = 50;
+var userEntered = opinion.length ; 
+var charLeft = totalChar - userEntered ;
+console.log(" You wrote " + userEntered + " you have " + charLeft + " remaining");
+
+*/
+
+/* slice method
+ The slice method in JavaScript takes two arguments: the starting index (inclusive) and the ending index (exclusive). In this case, it starts at index 0 and ends before index 1, so it extracts only the character at index 0. */
+
+//  var name = "Aamir";
+// var slicedPortion = name.slice(0, 1);
+// console.log(slicedPortion);
+
+// ********** Challenge by Angela**** This works in browser as it has alert and prompt function********
+
+// var Name = prompt ("What is your firstName");
+
+// var CapitaliseFirst = Name.slice(0,1);
+// var restName = Name.slice(1,Name.length)
+// var CapitalA = CapitaliseFirst.toUpperCase()
+
+//  final = "Hello " + CapitalA+ restName.toLowerCase()
+// alert(final)
+
+
---
+++ 01_basics/variableandstring.js
@@ -0,0 +1,35 @@
+
+
+// old method
+// console.log ("Instructor name is" + name)
+// console.log ("Instructor name is" + " "+ name)
+
+
+// old method
+
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+
+
+// const firstName = "Aamir"
+
+// const SecondName = "Sohail"
+
+// let myHobby = "Programming"
+
+// console.log(`Hey, I am ${firstName} ${SecondName} and my hobby is ${myHobby}`)
+
+const gameName = new String('hitesh-hc-com')
+console.log(gameName)
+
+
+
+
+
