543beb3297423643ea5ee0560ea1ffad0d4f68eb
aamirec001 <46120517+aamirec001@users.noreply.github.com>
2023-12-22 21:38:38 +0530

    added new comment
    added comment

--- 01_basics/variableandstring.js
+++ 01_basics/variableandstring.js
@@ -5,14 +5,16 @@
 // console.log ("Instructor name is" + " "+ name)
 
 
-// old method
+
 
 const name = "hitesh"
 const repoCount = 50
 
+// old method to print or output
+
 // console.log(name + repoCount + " Value");
 
-
+// New method to print or output. Notice the difference in line 13 and 18.
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 
@@ -26,6 +28,7 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
 // console.log(`Hey, I am ${firstName} ${SecondName} and my hobby is ${myHobby}`)
 
+// line 32 is another way of creating string
 const gameName = new String('hitesh-hc-com')
 console.log(gameName)
 
